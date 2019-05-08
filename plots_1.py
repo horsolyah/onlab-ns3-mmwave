e42ee488a928f5e9f59caef0b46602e5f4e05568
levesduzw <levente.sermaul@gmail.com>
2019-05-08 17:05:18 +0200

    Fork plot.py -> plot_multi.py

--- plots_1.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-traces = ['CWND', 'RCWND', 'RTT', 'DATA', 'LTESINR', 'MMWAVESINR']
-
-for trace in traces:
-    subprocess.call('python3 plot.py 1 {}'.format(trace), shell=True)
